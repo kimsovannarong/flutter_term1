ddf4362a9b8b63ae698160ccb13695b5dca1acc0
kimsovannarong <kimsovannarong590@gmail.com>
2024-10-16 10:47:31 +0700

    Challenge4

--- W2-Dart OOP/Challeng4/challenge4.dart
+++ W2-Dart OOP/Challeng4/challenge4.dart
@@ -1,13 +1,24 @@
 class Distance{
   final double distance;
-  Distance(this.distance);
+  final double distance_ms;
+  Distance.cms(this.distance):distance_ms = distance/100;
+  Distance.ms(this.distance):distance_ms = distance;
+  Distance.kms(this.distance):distance_ms = distance*1000;
+  double get cms{
+    return distance_ms * 100;
+  }
+    double get ms{
+    return distance_ms;
+  }
+    double get kms{
+    return distance_ms /1000;
+  }
   Distance operator+( Distance other){
-     return Distance(this.distance + other.distance); 
+     return Distance.ms(this.distance_ms + other.distance_ms); 
   }
 }
 void main() {
-  Distance d1=Distance(1);
-  Distance d2=Distance(2);
-  Distance d3=d1+d2;
-  print(d3.distance);
+  Distance d1=Distance.kms(3.4);
+  Distance d2=Distance.ms(1000);
+  print((d1+d2).kms);
 }
